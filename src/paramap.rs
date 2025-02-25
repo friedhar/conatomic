e3250c3837e228bfda823b6325565c1671f0dae5
friedhar <ronfrie@proton.me>
2025-02-25 11:38:05 +0200

    add, mod, del: paramap.rs

--- src/paramap.rs
+++ src/paramap.rs
@@ -1,1 +1,10 @@
+use std::collections::HashMap;
 
+use crate::spsc::Receiver;
+
+pub struct ParamapMessage {}
+
+pub struct Paramap<K, V> {
+    inner: HashMap<K, V>,
+    receiver: Receiver<ParamapMessage>,
+}
